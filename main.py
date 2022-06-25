80dcff82cea84fccea8fa37e94926102aa0913f4
DESKTOP-LBRP178\TomekS98 <tomekstachelo@gmail.com>
2022-06-25 23:39:38 +0200

    deleted multithreading changes, added ability to concatenate up to 6 streams

--- main.py
+++ main.py
@@ -2,17 +2,13 @@ import cv2
 import numpy as np
 import sys
 import _thread as thread
-import queue
-import threading
-import time
+
 rtsp_stream_paths = []
 
 video_capture = []
 status_of_captured_frames = []
 captured_frames = []
 opened_streams_count = 0
-
-
 def init():
     for path in rtsp_stream_paths:
         video_capture.append(cv2.VideoCapture(path))
@@ -21,61 +17,52 @@ def init():
         input_shape = np.shape(video.read()[1])
         captured_frames.append(np.empty(shape=input_shape))
 
-
-def resize_frame(frame, resolution):
-    frame = cv2.resize(frame, resolution)
 def get_output(opened_streams_count, opened_streames_indexes):
-    threads = []
     if opened_streams_count == 1:
         resolution = (1280, 720)
-        t1 = threading.Thread(target=resize_frame, args=(captured_frames[opened_streames_indexes[0]], resolution))
-        t1.start()
-        t1.join()
+        captured_frames[opened_streames_indexes[0]] = cv2.resize(captured_frames[opened_streames_indexes[0]],
+                                                                 resolution)
         return captured_frames[opened_streames_indexes[0]]
     elif opened_streams_count == 2:
         resolution = (768, 432)
-        for index, frame in enumerate(opened_streames_indexes):
-            t = threading.Thread(target=resize_frame, args=(captured_frames[opened_streames_indexes[index]], resolution,))
-            threads.append(t)
-            threads[index].start()
-
-        for thread in threads:
-            thread.join()
-
-        return np.concatenate(
-            tuple((captured_frames[opened_streames_indexes[index]] for index in opened_streames_indexes)), axis=1)
+        return np.concatenate(tuple((cv2.resize(captured_frames[index], resolution) for index in opened_streames_indexes)),
+                                 axis=1)
     elif opened_streams_count == 3:
         resolution = (512, 288)
-        for index, frame in enumerate(opened_streames_indexes):
-            t = threading.Thread(target=resize_frame,
-                                 args=(captured_frames[opened_streames_indexes[index]], resolution,))
-            threads.append(t)
-            threads[index].start()
-
-        for thread in threads:
-            thread.join()
-
-        return np.concatenate(
-            tuple((captured_frames[opened_streames_indexes[index]] for index in opened_streames_indexes)), axis=1)
+        return np.concatenate(tuple((cv2.resize(captured_frames[index], resolution) for index in opened_streames_indexes)),
+                                 axis=1)
+    elif opened_streams_count == 4:
+        resolution = (640, 360)
+        frames = list(captured_frames[index] for index in opened_streames_indexes)
+        for index, frame in enumerate(frames):
+            frames[index] = cv2.resize(frame, resolution)
+        ostream1 = np.concatenate((frames[0], frames[1]), axis=0)
+        ostream2 = np.concatenate((frames[2], frames[3]), axis=0)
+        return np.concatenate((ostream1, ostream2), axis=1)
+    elif opened_streams_count == 5:
+        resolution = (480, 270)
+        resolution2 = (960, 540)
+        frames = list(captured_frames[index] for index in opened_streames_indexes)
+        for index, frame in enumerate(frames[:-1]):
+            frames[index] = cv2.resize(frame, resolution)
+        frames[-1] = cv2.resize(frames[-1], resolution2)
+        ostream1 = np.concatenate((frames[0], frames[1]), axis=0)
+        ostream2 = np.concatenate((frames[2], frames[3]), axis=0)
+        ostream3 = np.concatenate((ostream1, ostream2), axis=1)
+        return np.concatenate((ostream3, frames[-1]), axis=0)
     else:
         resolution = (640, 360)
-        for index, frame in enumerate(captured_frames):
-            t = threading.Thread(target=resize_frame, args=(captured_frames[index], resolution,))
-            threads.append(t)
-            threads[index].start()
-
-        for thread in threads:
-            thread.join()
-
-        ostream1 = np.concatenate((captured_frames[0], captured_frames[1]), axis=0)
-        ostream2 = np.concatenate((captured_frames[2], captured_frames[3]), axis=0)
+        frames = list(captured_frames[index] for index in opened_streames_indexes)
+        for index, frame in enumerate(frames):
+            frames[index] = cv2.resize(frame, resolution)
+        ostream1 = np.concatenate((frames[0], frames[1], frames[4]), axis=0)
+        ostream2 = np.concatenate((frames[2], frames[3], frames[5]), axis=0)
         return np.concatenate((ostream1, ostream2), axis=1)
 
 
 def renew_connection(index):
     global video_capture
     video_capture[index] = cv2.VideoCapture(rtsp_stream_paths[index])
-
 
 def connect_inputs():
     while any(video.isOpened() for video in video_capture):
@@ -99,13 +86,12 @@ def connect_inputs():
 def main():
     global rtsp_stream_paths
     args = []
-    for arg in sys.argv:
+    for arg in sys.argv[1:]:
         args.append(arg)
-    rtsp_stream_paths = args[1:]
+    rtsp_stream_paths = args
     init()
     connect_inputs()
     cv2.destroyAllWindows()
-
 
 if __name__ == '__main__':
     sys.exit(main())
